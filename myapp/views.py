07dc94034c74be53ed54c7ef6ef0c2a5feddc53b
maxmillan45 <maxmillannjue@gmail.com>
2026-05-05 20:59:35 +0300

    add paths on views page

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'Zindua/home.html')
+
+def about(request):
+    return render(request, 'Zindua/about.html')
+
+def contact(request):
+    return render(request, 'Zindua/contact.html')
+
+def programmes(request):
+    context = {
+        'courses': ['Software Engineering', 'Data Science', 'Cybersecurity', 'Product Management'],
+        'updated_date': '2023-10-01'
+    }
+    return render(request, 'Zindua/programmes.html', context)
